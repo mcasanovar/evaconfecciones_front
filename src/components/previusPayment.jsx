c9b7354d019af0155f3eae83a13ea19de56ea2b1
mario <mariocasanovaramirez@gmail.com>
2022-01-08 18:27:52 -0300

    agregada una referencia de monto formateado

--- src/components/previusPayment.jsx
+++ src/components/previusPayment.jsx
@@ -12,34 +12,39 @@ const PreviusPaymentComponent = ({
   plusMinus
 }) => {
   return (
-    <div className="w-full flex justify-end items-center pr-4">
-      <h1 className="font-bold text-black pr-4 flex justify-end uppercase">abono</h1>
-      <InputComponent
-        id="previusPayment"
-        type="number"
-        placeholder="Pago de Abono"
-        width="40"
-        value={previusPayment}
-        onChange={(e) => setPreviusPayment(e.target.value)}
-      />
-      <ButtonComponent
-        color="green"
-        text="+"
-        width="20"
-        height="9"
-        icon=""
-        onClick={() => plusMinus("plus")}
-      />
-      <ButtonComponent
-        color="red"
-        text="-"
-        width="20"
-        height="9"
-        icon=""
-        onClick={() => plusMinus("minus")}
-      />
-      <h1 className="font-bold text-green-900 pl-1 text-2xl flex justify-end uppercase">{`${formattedPrices(totalPreviusPayment)}`}</h1>
-    </div>
+    <>
+      <div className="w-full flex justify-end items-start pr-4">
+        <h1 className="font-bold text-black pr-4 flex justify-end uppercase">abono</h1>
+        <div className="">
+          <InputComponent
+            id="previusPayment"
+            type="number"
+            placeholder="Pago de Abono"
+            width="40"
+            value={previusPayment}
+            onChange={(e) => setPreviusPayment(e.target.value)}
+          />
+          <h1 className="font-bold text-black pr-4 flex justify-start uppercase pl-1 pt-1">{formattedPrices(previusPayment)}</h1>
+        </div>
+        <ButtonComponent
+          color="green"
+          text="+"
+          width="20"
+          height="9"
+          icon=""
+          onClick={() => plusMinus("plus")}
+        />
+        <ButtonComponent
+          color="red"
+          text="-"
+          width="20"
+          height="9"
+          icon=""
+          onClick={() => plusMinus("minus")}
+        />
+        <h1 className="font-bold text-green-900 pl-1 text-2xl flex justify-end uppercase">{`${formattedPrices(totalPreviusPayment)}`}</h1>
+      </div>
+    </>
   )
 }
 
